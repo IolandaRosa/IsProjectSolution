436ff5f1f7a0c8d6268f32b1aa7761c069dafc2e
Iolanda Rosa <2161477@my.ipleiria.pt>
2018-11-21 23:06:19 +0000

    Add project files.

---
+++ BOT-SpotSensors/IServiceSpotSensor.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Xml;
+
+namespace BOT_SpotSensors
+{
+    [ServiceContract]
+    public interface IServiceSpotSensor
+    {
+
+        [OperationContract]
+        List<Spot> GetAllSpotsData();
+
+        [OperationContract]
+        XmlElement GetAllSpotsIDataXml();
+
+    }
+
+    [DataContract]
+    public class Spot
+    {
+        [DataMember]
+        public String Id { get; set; }
+
+        [DataMember]
+        public String Type { get; set; }
+
+        [DataMember]
+        public String Name { get; set; }
+
+        [DataMember]
+        public String Location { get; set; }
+
+        [DataMember]
+        public Status Status { get; set; }
+
+        [DataMember]
+        public int BatteryStatus { get; set; }
+    }
+
+    [DataContract]
+    public class Status
+    {
+        [DataMember]
+        public ValueType Value { get; set; }
+
+        [DataMember]
+        public DateTime Timestamp { get; set; }
+    }
+
+    public enum ValueType
+    {
+        FREE, OCCUPIED
+    }
+}
---
+++ BOT-SpotSensors/ServiceSpotSensor.svc.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+using System.Xml;
+
+namespace BOT_SpotSensors
+{
+    public class ServiceSpotSensor : IServiceSpotSensor
+    {
+        string filePath = AppDomain.CurrentDomain.BaseDirectory.ToString() + @"App_Data\XMLPark_B_Spots.xml";
+
+        public List<Spot> GetAllSpotsData()
+        {
+            List<Spot> spots = new List<Spot>();
+
+            XmlDocument doc = new XmlDocument();
+            doc.Load(filePath);
+
+            XmlNodeList xmlSpots = doc.SelectNodes("/parkSpots/parkingSpot");
+
+            foreach (XmlNode item in xmlSpots)
+            {
+                Spot spot = new Spot();
+
+                spot.Id = item["id"].InnerText;
+                spot.Type = item["type"].InnerText;
+                spot.Location = item["location"].InnerText;
+                spot.Name = item["name"].InnerText;
+                spot.BatteryStatus = int.Parse(item["batteryStatus"].InnerText);
+
+                XmlNode statusNode = item.SelectSingleNode("status");
+
+                if (statusNode != null)
+                {
+                    spot.Status = new Status
+                    {
+                        Value= (ValueType)Enum.Parse(typeof(ValueType), statusNode["value"].InnerText.ToUpper()),
+                        Timestamp = DateTime.ParseExact(statusNode["timestamp"].InnerText, "yyyy-MM-dd HH:mm:ss", CultureInfo.InvariantCulture)
+                    };
+                }
+
+                spots.Add(spot);
+            }
+
+            return spots;
+        }
+
+        public XmlElement GetAllSpotsIDataXml()
+        {
+            XmlDocument doc = new XmlDocument();
+            doc.Load(filePath);
+
+            return doc.DocumentElement;
+        }
+    }
+}
